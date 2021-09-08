a67757242bddedeaaad95e4b267e636bb1a21079
kent99821 <2270514313@qq.com>
2021-09-08 20:40:44 +0800

    添加文章的markdown效果

--- src/pages/AddArticle.js
+++ src/pages/AddArticle.js
@@ -1,13 +1,50 @@
 import React, { useState } from "react";
-import marked from 'marked';
 import '../assets/css/AddArticle.css';
 import { Row, Col, Input, Select, Button, DatePicker } from "antd";
+import marked from 'marked';
+import hljs from 'highlight.js';
+import 'highlight.js/styles/monokai-sublime.css';
 
 
 const { Option } = Select;
 const { TextArea } = Input;
 
 function AddArticle() {
+    const [articleId, setArticleId] = useState(0);//文章id 如果0说明新增的
+    const [articleTitle, setArticleTitle] = useState("");//文章标题
+    const [articleContent, setArticlecontent ] = useState("");//markdown的编辑内容
+    const [markdownContent, setMarkdownContent] = useState("预览内容");//html内容
+    const [introducemd, setIntroducemd] = useState();//简介的markdown内容
+    const [introducehtml, setIntroducehtml] = useState("等待编辑"); //简介的html内容
+    const [showDate, setShowDate] = useState();//发布日期
+    const [updateDate, setUpdateDate] = useState();//修改的日期
+    const [typeInfo, setTypeInfo] = useState([]); //文章类别信息
+    const [selectedType, setSelectType] = useState(1);//选择的文章类别
+    const renderer = new marked.Renderer();
+  
+    marked.setOptions({
+        renderer:renderer,
+        gfm: true,
+        pedantic: false,
+        sanitize: false,
+        tables: true,
+        breaks: true,
+        smartLists: true,
+        smartypants: true,
+        highlight: function (code) {
+                return hljs.highlightAuto(code).value;
+        }
+    });
+    const changeContent = (e)=>{
+        setArticlecontent(e.target.value);
+        let html = marked(e.target.value);
+        setMarkdownContent(html);
+    }
+    const changeIntroduce = (e)=>{
+        setIntroducemd(e.target.value);
+        let html = marked(e.target.value);
+        setIntroducehtml(html);
+    }
     return (
 
         <div>
@@ -27,10 +64,14 @@ function AddArticle() {
                     <br />
                     <Row gutter={10}>
                         <Col span={12}>
-                            <TextArea className="markdown-content" rows={35} placeholder="文章内容" />
+                            <TextArea className="markdown-content" 
+                            rows={35} placeholder="文章内容" onChange={changeContent}/>
                         </Col>
                         <Col span={12}>
-                            <div className="show-html">
+                            <div className="show-html" 
+                            dangerouslySetInnerHTML={{__html:markdownContent}}
+                            >
+
                             </div>
                         </Col>
                     </Row>
@@ -47,9 +88,12 @@ function AddArticle() {
                             <TextArea
                                 rows={4}
                                 placeholder="文章简介"
+                                onChange={changeIntroduce}
                             />
                             <br /><br />
-                            <div className="introduce-html"></div>
+                            <div className="introduce-html"
+                            dangerouslySetInnerHTML={{__html:"文章简介："+ introducehtml}}
+                            ></div>
                         </Col>
 
                         <Col span={12}>
