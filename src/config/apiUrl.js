21b730e11aa8de4193e0192086ae01f9d682ff97
kent99821 <2270514313@qq.com>
2021-09-20 20:17:07 +0800

    删除文章功能

--- src/config/apiUrl.js
+++ src/config/apiUrl.js
@@ -4,6 +4,10 @@ let servicePath = {
    checkLogin: ipUrl + 'checkOpenId',//检查
    getTypeInfo: ipUrl + 'getTypeInfo',//获取文章类别
    addArticle: ipUrl + 'addArticle',//添加文章
-   updateArticle: ipUrl + 'updateArticle' //更新文章
+   updateArticle: ipUrl + 'updateArticle', //更新文章
+   getArticleList: ipUrl + 'getArticleList', //获取文章列表
+   delArticle: ipUrl + 'delArticle' //删除文章
+
+   
 }
 export default servicePath
--- src/pages/ArticleList.js
+++ src/pages/ArticleList.js
@@ -2,62 +2,100 @@ import React, { useState, useEffect } from "react";
 import { List, Row, Col, Modal, message, Button } from 'antd';
 import axios from "axios";
 import servicePath from "../config/apiUrl";
+import '../assets/css/ArticleList.css'
 const { confirm } = Modal;
 
 function ArticleList(props) {
     const [list, setList] = useState([]);
-    return (
-        <div>
-            <List
-                header={
-                    <Row className="list-div">
-                        <Col span={8}>
-                            <b>标题</b>
-                        </Col>
-                        <Col span={4}>
-                            <b>类别</b>
-                        </Col>
-                        <Col span={4}>
-                            <b>发布时间</b>
-                        </Col>
-                        <Col span={4}>
-                            <b>浏览量</b>
-                        </Col>
-                        <Col span={4}>
-                            <b>操作</b>
-                        </Col>
-                    </Row>
+    const getList = () => {
+        axios({
+            method: 'get',
+            url: servicePath.getArticleList,
+            withCredentials: true
+        }).then(
+        res => {
+            setList(res.data.list);
+        })
+}
+const delArticle = (id)=>{
+    confirm({
+        title:'确定删除这篇文章吗？',
+        content:'点击确认按钮后文章将会永远删除',
+        okText:'确定',
+        cancelText:'取消',
+        onOk(){
+            axios({
+                method:'post',
+                url:servicePath.delArticle,
+                data:{'id':id},
+                withCredentials:true
+            }).then(
+                res=>{
+                    message.success('文章删除成功');
+                    getList();
                 }
-                bordered
-                dataSource={list}
-                renderItem={item => (
-                    <List.Item>
+            )
+        },
+        onCancel(){
+            message.success('已取消操作');
+        }
+    })
+}
+useEffect(() => {
+    getList();
+}, [])
+return (
+    <div>
+        <List
+            header={
+                <Row className="list-div">
+                    <Col span={8}>
+                        <b>标题</b>
+                    </Col>
+                    <Col span={4}>
+                        <b>类别</b>
+                    </Col>
+                    <Col span={4}>
+                        <b>发布时间</b>
+                    </Col>
+                    <Col span={4}>
+                        <b>浏览量</b>
+                    </Col>
+                    <Col span={4}>
+                        <b>操作</b>
+                    </Col>
+                </Row>
+            }
+            bordered
+            dataSource={list}
+            renderItem={item => (
+                <List.Item>
                     <Row className="list-div">
                         <Col span={8}>
                             {item.title}
                         </Col>
                         <Col span={4}>
-                        {item.typeName}
+                            {item.typeName}
                         </Col>
                         <Col span={4}>
-                        {item.addTime}
+                            {item.addTime}
                         </Col>
                         <Col span={4}>
-                        {item.view_content}
+                            {item.view_count}
                         </Col>
                         <Col span={4}>
-                        <Button type="primary">修改</Button>
-                        <Button type="primary" danger>删除</Button>
+                            <Button className="act" type="primary">修改</Button>
+                            <Button className="act" type="primary" onClick={()=>{delArticle(item.id)}} danger>删除</Button>
                         </Col>
                     </Row>
                 </List.Item>
-                )}
+            )}
 
 
-            />
+        />
 
-        </div>
-    )
+    </div>
+)
 }
 
 export default ArticleList;
